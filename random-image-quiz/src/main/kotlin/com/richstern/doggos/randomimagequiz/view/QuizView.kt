b6a8bbf0535d63c9ae22277fa4eed7c76d2cf76b
Rich Stern <rich.stern@gotinder.com>
2020-08-06 15:11:39 -0700

    Image and styling on quiz view

--- random-image-quiz/src/main/kotlin/com/richstern/doggos/randomimagequiz/view/QuizView.kt
+++ random-image-quiz/src/main/kotlin/com/richstern/doggos/randomimagequiz/view/QuizView.kt
@@ -2,19 +2,25 @@ package com.richstern.doggos.randomimagequiz.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.ScrollView
+import com.bumptech.glide.Glide
 import com.richstern.doggos.randomimagequiz.R
 import com.richstern.doggos.randomimagequiz.statemachine.QuizState
 
 class QuizView(context: Context, attrs: AttributeSet) : ScrollView(context, attrs) {
+
+    private val imageView by lazy { findViewById<ImageView>(R.id.quiz_view_image) }
 
     init {
         View.inflate(context, R.layout.view_quiz, this)
     }
 
     fun bind(quizState: QuizState.RandomImageLoaded) {
-        Log.d("Rich", "bind: ${quizState.randomImage}")
+        Glide.with(this)
+            .load(quizState.randomImage.imageUrl)
+            .centerCrop()
+            .into(imageView)
     }
 }
