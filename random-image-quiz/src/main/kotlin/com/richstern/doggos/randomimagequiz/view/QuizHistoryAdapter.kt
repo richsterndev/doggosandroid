e290d66885976adb9154b387482217a86cd1ae53
Rich Stern <rich.stern@gotinder.com>
2020-08-09 10:42:51 -0700

    Hook up recycler view adapter

---
+++ random-image-quiz/src/main/kotlin/com/richstern/doggos/randomimagequiz/view/QuizHistoryAdapter.kt
@@ -0,0 +1,48 @@
+package com.richstern.doggos.randomimagequiz.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.richstern.doggos.model.Breed
+import com.richstern.doggos.randomimagequiz.R
+
+class QuizHistoryAdapter : RecyclerView.Adapter<QuizHistoryAdapter.HistoryItemViewHolder>() {
+
+    private val historyItems = mutableListOf<Breed>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.view_history_item, parent, false)
+        return HistoryItemViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return historyItems.size
+    }
+
+    override fun onBindViewHolder(holder: HistoryItemViewHolder, position: Int) {
+        val item = historyItems[position]
+        holder.bind(item)
+    }
+
+    fun setItems(items: List<Breed>) {
+        historyItems.clear()
+        historyItems.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    class HistoryItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val nameTextView by lazy {
+            itemView.findViewById<TextView>(R.id.history_item_breed_name)
+        }
+
+        fun bind(item: Breed) {
+            nameTextView.text = item.name
+        }
+
+    }
+}
--- random-image-quiz/src/main/kotlin/com/richstern/doggos/randomimagequiz/view/QuizHistoryView.kt
+++ random-image-quiz/src/main/kotlin/com/richstern/doggos/randomimagequiz/view/QuizHistoryView.kt
@@ -2,17 +2,24 @@ package com.richstern.doggos.randomimagequiz.view
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.richstern.doggos.randomimagequiz.R
 import com.richstern.doggos.randomimagequiz.statemachine.QuizHistoryState
 
 class QuizHistoryView(context: Context, attrs: AttributeSet) : SwipeRefreshLayout(context, attrs) {
 
+    private val recyclerView by lazy { findViewById<RecyclerView>(R.id.quiz_history_list) }
+    private val adapter = QuizHistoryAdapter()
+
     init {
         inflate(context, R.layout.view_quiz_history, this)
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.adapter = adapter
     }
 
     fun bind(quizHistoryState: QuizHistoryState.HistoryLoaded) {
-
+        adapter.setItems(quizHistoryState.list)
     }
 }
